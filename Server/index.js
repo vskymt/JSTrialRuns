a84ad99627ef69d7e2688f0acdb0e0755e97a312
Vamshi Yamato <vskymt@gmail.com>
2021-07-12 03:21:55 +0530

    OOPS Learning

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,23 @@
-console.log('Vamshi Yamato');
+let count = {
+    first: 1,
+    second: 2,
+    third: 3,
+
+    sum: function() {
+        return this.first + this.second + this.third;
+    }
+};
+let p = count.sum();
+console.log(p);
+
+let circle = {
+    radius: 1,
+    locatio: {
+        x:1,
+        y:1
+    },
+    draw: function() {
+        console.log ('draw');
+    }
+};
+circle.draw();
