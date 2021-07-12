c64b9077bbfcc1bea622670d7cf96dd646903e52
Vamshi Yamato <vskymt@gmail.com>
2021-07-13 04:51:35 +0530

    OOPS Learning till Factories and Constructors

--- Server/index.js
+++ Server/index.js
@@ -21,3 +21,37 @@ let circle = {
     }
 };
 circle.draw();
+
+function createCircle (radius) {
+    return {
+    //radius, is radius=radius, (update)
+    radius,
+    draw: function () {
+        console.log('draw');
+    }
+    };
+}
+let circle2 = createCircle(1);
+circle2.draw();
+
+function Circle(radius) {
+    console.log('this',this);
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+let another = new Circle(1);
+another.draw();
+console.log(another.constructor);
+console.log(circle2.constructor);
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
+
+const Circle1 = new Function('radius', `console.log('this',this);
+this.radius = radius;
+this.draw = function() {
+    console.log('draw');
+}`);
+let circle3 = new Circle1(1);
+circle3.draw();
